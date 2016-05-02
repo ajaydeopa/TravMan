a6113db556c53a5de76aa23f1bb6b3aa367044d5
Ajay Singh Deopa <singhdeopa>
2016-05-02 22:32:59 +0530

    changed index page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,17 +3,7 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-<<<<<<< HEAD
-                In all new pages we have to now use this
 
-                <h1>extends('layouts.app',['link' => 'Add URL'])</h1> now we have to extend app.blade not template.first
-                <br>
-                <br>
-                <h3> and please define in comment where there is ajax call and other function</h3>
-
-                <h2>run composer install and composer update </h2>
-=======
->>>>>>> origin/master
             </div>
         </div>
     </div>
